376ff8fd31b2409a59192b3e993069b8c3c63620
AUDREY KADJAR <audreykadjar@gmail.com>
2020-04-14 13:29:11 +0200

    saving edit to text and README

--- README.md
+++ README.md
@@ -5,10 +5,10 @@
 
 This React app enables users to express their feelings about the COVID-19 outbreak and browse through others' submissions.<br /> <br />
 This project's intention is to collect personal, emotional, yet global, perspectives on the situation.
-When all this is over, I'm hoping that it could serve as a digital
+When all this is over, I hope that it could serve as a digital
 archive reminding us of this very strange time.
 <br /> <br />
 FEATURES: submit form & browse section picking random submissions <br />
 Technologies: HTML, CSS, JavaScript, Node with Express.js, PostgreSQL <br />
 Test: React Testing Library + Jest <br />
-Framework: React
+Framework: React | Deployed on Heroku
--- src/info.js
+++ src/info.js
@@ -2,24 +2,28 @@ import React from "react";
 import MetaTags from "react-meta-tags";
 
 export default function Info() {
-  return (
-    <div className="info">
-      <MetaTags>
-        <title>corona emotions club</title>
-        <meta name="description" content="COVID-19 emotions & feelings" />
-        <meta property="og:title" content="corona emotions club" />
-      </MetaTags>
-      <p className="info_text">
-        This website is a collective gathering of emotions related to the
-        COVID-19 outbreak. <br /> <br /> You can submit all the fears, dreams,
-        hopes you have regarding the situation and browse through the collection
-        to see others’ submissions. <br /> <br /> This website’s intention is to
-        collect personal, emotional, yet global, perspectives on the situation.
-        When all this is over, I'm hoping that it could serve as a digital
-        archive reminding us of this very strange time.
-        <br /> <br />
-        website made by Audrey Kadjar
-      </p>
-    </div>
-  );
+    return (
+        <div className="info">
+            <MetaTags>
+                <title>corona emotions club</title>
+                <meta
+                    name="description"
+                    content="COVID-19 emotions & feelings"
+                />
+                <meta property="og:title" content="corona emotions club" />
+            </MetaTags>
+            <p className="info_text">
+                This website is a collective gathering of emotions related to
+                the COVID-19 outbreak. <br /> <br /> You can submit all the
+                fears, dreams, hopes you have regarding the situation and browse
+                through the collection to see others’ submissions. <br /> <br />
+                This website’s intention is to collect personal, emotional, yet
+                global, perspectives on the situation. When all this is over, I
+                hope that it could serve as a digital archive reminding us of
+                this very strange time.
+                <br /> <br />
+                website made by Audrey Kadjar
+            </p>
+        </div>
+    );
 }
