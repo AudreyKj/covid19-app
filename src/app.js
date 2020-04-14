d4af7d5b16ecca4cc623cd859d371fb234dcbfe6
AUDREY KADJAR <audreykadjar@gmail.com>
2020-04-14 10:10:37 +0200

    edits to responsive design

--- src/app.js
+++ src/app.js
@@ -6,72 +6,83 @@ import Browse from "./browse";
 import Info from "./info";
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  componentDidMount() {
-    //ticker
-    const title = "&nbsp;&nbsp;CORONA EMOTIONS CLUB&nbsp;&nbsp;";
-    const ticker = new Array(200).fill(title);
+    componentDidMount() {
+        //ticker
+        const title = "&nbsp;&nbsp;CORONA EMOTIONS CLUB&nbsp;&nbsp;";
+        const ticker = new Array(200).fill(title);
 
-    const emotionTicker = document.querySelector("div.ticker h1");
-    emotionTicker.innerHTML = ticker.join("");
+        const emotionTicker = document.querySelector("div.ticker h1");
+        emotionTicker.innerHTML = ticker.join("");
 
-    //cursor
-    const cursor = document.querySelector("div.cursor");
+        //cursor
+        const cursor = document.querySelector("div.cursor");
 
-    document.addEventListener("mousemove", function(e) {
-      cursor.style.top = e.clientY - 10 + "px";
-      cursor.style.left = e.clientX - 10 + "px";
-    });
-  }
+        document.addEventListener("mousemove", function(e) {
+            cursor.style.top = e.clientY - 10 + "px";
+            cursor.style.left = e.clientX - 10 + "px";
+        });
 
-  render() {
-    return (
-      <>
-        <div className="gradient"></div>
-        <div className="cursor"></div>
-        <div className="ticker">
-          <h1>
-            CORONA EMOTIONS CLUB &nbsp;&nbsp; CORONA EMOTIONS CLUB &nbsp;&nbsp;
-          </h1>
-        </div>
+        document.addEventListener("touchmove", function(e) {
+            cursor.style.top = e.clientY - 10 + "px";
+            cursor.style.left = e.clientX - 10 + "px";
+        });
+    }
 
-        <BrowserRouter>
-          <nav>
-            <NavLink
-              className="link-menu"
-              activeClassName="active"
-              to="/submit"
-            >
-              submit
-            </NavLink>
-            &nbsp;&nbsp;
-            <NavLink
-              className="link-menu"
-              activeClassName="active"
-              to="/browse"
-            >
-              browse
-            </NavLink>
-            &nbsp;&nbsp;
-            <NavLink className="link-menu" activeClassName="active" to="/info">
-              info
-            </NavLink>
-            &nbsp;&nbsp;
-            <span className="instructions">
-              🦠 how do you feel about the COVID-19 crisis? let it all out!
-            </span>
-          </nav>
+    render() {
+        return (
+            <>
+                <div className="gradient"></div>
+                <div className="cursor"></div>
+                <div className="ticker">
+                    <h1>
+                        CORONA EMOTIONS CLUB &nbsp;&nbsp; CORONA EMOTIONS CLUB
+                        &nbsp;&nbsp;
+                    </h1>
+                </div>
 
-          <Route path="/submit" component={Submit}></Route>
+                <BrowserRouter>
+                    <nav>
+                        <NavLink
+                            className="link-menu"
+                            activeClassName="active"
+                            to="/submit"
+                        >
+                            submit
+                        </NavLink>
+                        &nbsp;&nbsp;
+                        <NavLink
+                            className="link-menu"
+                            activeClassName="active"
+                            to="/browse"
+                        >
+                            browse
+                        </NavLink>
+                        &nbsp;&nbsp;
+                        <NavLink
+                            className="link-menu"
+                            activeClassName="active"
+                            to="/info"
+                        >
+                            info
+                        </NavLink>
+                        &nbsp;&nbsp;
+                        <span className="instructions">
+                            🦠 how do you feel about the COVID-19 crisis? let it
+                            all out!
+                        </span>
+                    </nav>
 
-          <Route path="/browse" component={Browse}></Route>
+                    <Route path="/submit" component={Submit}></Route>
 
-          <Route path="/info" component={Info}></Route>
-        </BrowserRouter>
-      </>
-    );
-  }
+                    <Route path="/browse" component={Browse}></Route>
+
+                    <Route path="/info" component={Info}></Route>
+                </BrowserRouter>
+            </>
+        );
+    }
 }
--- src/browse.js
+++ src/browse.js
@@ -4,110 +4,121 @@ import axios from "./axios";
 import MetaTags from "react-meta-tags";
 
 export default function Browse() {
-  const [randomEmotion, setRandomEmotion] = useState();
-  const [color, setColor] = useState();
-  useEffect(() => {
-    (async () => {
-      const { data } = await axios.get("/getEmotion");
-      const result = [];
+    const [randomEmotion, setRandomEmotion] = useState();
+    const [color, setColor] = useState();
+    useEffect(() => {
+        (async () => {
+            const { data } = await axios.get("/getEmotion");
+            const result = [];
 
-      result.push(data);
+            result.push(data);
 
-      setRandomEmotion(result);
+            setRandomEmotion(result);
 
-      const colorPalette = [
-        "rgba(255, 115,	115, 0.7)",
-        "rgba(219,145,169, 0.7)",
-        "rgba(160,212,216, 0.9)",
-        "rgba(102,205,170, 0.7)",
-        "rgba(153,102,255, 0.7)",
-        "rgba(51,153,255, 0.7)",
-        "rgba(97,174,251, 0.7)",
-        "rgba(70,132,153, 0.7)",
-        "rgba(255,165,0, 0.7)",
-        "rgba(218,165,32, 0.7)",
-        "rgba(255, 0, 0, 0.7)",
-        "rgba(0, 177, 89, 0.7)"
-      ];
+            const colorPalette = [
+                "rgba(255, 115,	115, 0.7)",
+                "rgba(219,145,169, 0.7)",
+                "rgba(160,212,216, 0.9)",
+                "rgba(102,205,170, 0.7)",
+                "rgba(153,102,255, 0.7)",
+                "rgba(51,153,255, 0.7)",
+                "rgba(97,174,251, 0.7)",
+                "rgba(70,132,153, 0.7)",
+                "rgba(255,165,0, 0.7)",
+                "rgba(218,165,32, 0.7)",
+                "rgba(255, 0, 0, 0.7)",
+                "rgba(0, 177, 89, 0.7)"
+            ];
 
-      const randomColor = colorPalette[Math.floor(Math.random() * 12)];
+            const randomColor = colorPalette[Math.floor(Math.random() * 12)];
 
-      const style = {
-        newStyle: {
-          backgroundColor: randomColor,
-          boxShadow: `0px 0px 15px 10px ${randomColor}`
-        }
-      };
+            const style = {
+                newStyle: {
+                    backgroundColor: randomColor,
+                    boxShadow: `0px 0px 15px 10px ${randomColor}`
+                }
+            };
 
-      setColor(style.newStyle);
-    })();
+            setColor(style.newStyle);
+        })();
 
-    document.addEventListener("keydown", function(e) {
-      if (e.keyCode === 39) {
-        getNextEmotion();
-      }
-    });
-  }, []);
+        document.addEventListener("keydown", function(e) {
+            if (e.keyCode === 39) {
+                getNextEmotion();
+            }
+        });
+    }, []);
 
-  const getNextEmotion = e => {
-    axios.get("/getEmotion").then(function(data) {
-      const result = [];
+    const getNextEmotion = e => {
+        axios.get("/getEmotion").then(function(data) {
+            const result = [];
 
-      result.push(data.data);
+            result.push(data.data);
 
-      setRandomEmotion(result);
+            setRandomEmotion(result);
 
-      const colorPalette = [
-        "rgba(255	115	115, 0.7)",
-        "rgba(219,145,169, 0.7)",
-        "rgba(160,212,216, 0.9)",
-        "rgba(102,205,170, 0.7)",
-        "rgba(153,102,255, 0.7)",
-        "rgba(51,153,255, 0.7)",
-        "rgba(97,174,251, 0.7)",
-        "rgba(70,132,153, 0.7)",
-        "rgba(255,165,0, 0.7)",
-        "rgba(218,165,32, 0.7)",
-        "rgba(255, 0, 0, 0.7)",
-        "rgba(0, 177, 89, 0.7)"
-      ];
+            const colorPalette = [
+                "rgba(255	115	115, 0.7)",
+                "rgba(219,145,169, 0.7)",
+                "rgba(160,212,216, 0.9)",
+                "rgba(102,205,170, 0.7)",
+                "rgba(153,102,255, 0.7)",
+                "rgba(51,153,255, 0.7)",
+                "rgba(97,174,251, 0.7)",
+                "rgba(70,132,153, 0.7)",
+                "rgba(255,165,0, 0.7)",
+                "rgba(218,165,32, 0.7)",
+                "rgba(255, 0, 0, 0.7)",
+                "rgba(0, 177, 89, 0.7)"
+            ];
 
-      const randomColor = colorPalette[Math.floor(Math.random() * 12)];
+            const randomColor = colorPalette[Math.floor(Math.random() * 12)];
 
-      const style = {
-        newStyle: {
-          backgroundColor: randomColor,
-          boxShadow: `0px 0px 15px 10px ${randomColor}`
-        }
-      };
+            const style = {
+                newStyle: {
+                    backgroundColor: randomColor,
+                    boxShadow: `0px 0px 15px 10px ${randomColor}`
+                }
+            };
 
-      setColor(style.newStyle);
-    });
-  };
+            setColor(style.newStyle);
+        });
+    };
 
-  return (
-    <div className="randomEmotion">
-      <MetaTags>
-        <title>corona emotions club</title>
-        <meta name="description" content="COVID-19 emotions & feelings" />
-        <meta property="og:title" content="corona emotions club" />
-      </MetaTags>
-      {randomEmotion &&
-        randomEmotion.map(emotion => (
-          <div className="emotion-block" style={color} key={emotion.id}>
-            {emotion.emotion} <br />
-            <span className="country">
-              {emotion.country} <br />
-            </span>
-            <span className="date">
-              <Moment format="MMMM Do YYYY">{emotion.created_at}</Moment>
-            </span>
-          </div>
-        ))}
+    return (
+        <div className="randomEmotion-wrapper">
+            <div className="randomEmotion">
+                <MetaTags>
+                    <title>corona emotions club</title>
+                    <meta
+                        name="description"
+                        content="COVID-19 emotions & feelings"
+                    />
+                    <meta property="og:title" content="corona emotions club" />
+                </MetaTags>
+                {randomEmotion &&
+                    randomEmotion.map(emotion => (
+                        <div
+                            className="emotion-block"
+                            style={color}
+                            key={emotion.id}
+                        >
+                            {emotion.emotion} <br />
+                            <span className="country">
+                                {emotion.country} <br />
+                            </span>
+                            <span className="date">
+                                <Moment format="MMMM Do YYYY">
+                                    {emotion.created_at}
+                                </Moment>
+                            </span>
+                        </div>
+                    ))}
 
-      <div className="next" onClick={getNextEmotion}>
-        <span className="next-arrow"> → </span>
-      </div>
-    </div>
-  );
+                <div className="next" onClick={getNextEmotion}>
+                    <span className="next-arrow"> → </span>
+                </div>
+            </div>
+        </div>
+    );
 }
