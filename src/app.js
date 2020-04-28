3466fa61cb023b56af40983853636a88344e61a3
AUDREY KADJAR <audreykadjar@gmail.com>
2020-04-28 22:12:44 +0200

    saving responsive

--- src/app.js
+++ src/app.js
@@ -56,7 +56,9 @@ export default class App extends React.Component {
                                 }}
                             >
                                 CORONA EMOTIONS CLUB &nbsp;&nbsp; CORONA
-                                EMOTIONS CLUB &nbsp;&nbsp;
+                                EMOTIONS CLUB &nbsp;&nbsp; CORONA EMOTIONS CLUB
+                                &nbsp;&nbsp; CORONA EMOTIONS CLUB
+                                &nbsp;&nbsp;CORONA EMOTIONS CLUB &nbsp;&nbsp;
                             </h1>
                         )}
                     </Ticker>
