7ff4323c796d8139379b1479fffca1216a22c61a
AUDREY KADJAR <audreykadjar@gmail.com>
2020-07-27 16:16:54 +0200

    updated class component to function component

--- src/app.js
+++ src/app.js
@@ -9,117 +9,106 @@ import Info from "./info";
 import Admin from "./admin";
 import { Helmet } from "react-helmet";
 
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <>
-                <Helmet>
-                    <title>Corona Emotions Club</title>
-                    <meta name="description" content="COVID-19" />
-                    <meta
-                        name="keywords"
-                        content="COVID-19, corona virus, pandemic, emotions"
-                    />
-                    <meta
-                        name="application-name"
-                        content="corona emotions club"
-                    />
-                    <meta name="theme-color" content="green" />
-                    <meta property="og:image:width" content="1080" />
-                    <meta property="og:image:height" content="1080" />
-                    <meta
-                        property="og:url"
-                        content="https://corona-emotions.club/browse"
-                    />
-                    <meta
-                        property="og:description"
-                        content="corona emotions club"
-                    />
-                    <meta property="og:image" content="preview.jpg" />
-                    <meta property="og:image:url" content="preview.jpg" />
-                </Helmet>
-
-                <GoogleFontLoader
-                    fonts={[
-                        {
-                            font: "Codystar",
-                            weights: [400, "400i"]
-                        }
-                    ]}
+export default function App() {
+    return (
+        <>
+            <Helmet>
+                <title>Corona Emotions Club</title>
+                <meta name="description" content="COVID-19" />
+                <meta
+                    name="keywords"
+                    content="COVID-19, corona virus, pandemic, emotions"
                 />
+                <meta name="application-name" content="corona emotions club" />
+                <meta name="theme-color" content="green" />
+                <meta property="og:image:width" content="1080" />
+                <meta property="og:image:height" content="1080" />
+                <meta
+                    property="og:url"
+                    content="https://corona-emotions.club/browse"
+                />
+                <meta
+                    property="og:description"
+                    content="corona emotions club"
+                />
+                <meta property="og:image" content="preview.jpg" />
+                <meta property="og:image:url" content="preview.jpg" />
+            </Helmet>
 
-                <Ticker speed={4}>
-                    {({ index }) => (
-                        <h1
-                            style={{
-                                fontFamily: "Codystar",
-                                textShadow:
-                                    "0 0 0.1em #fff, 0 0 0.15em #fff, 0 0 0.2em #fff, 0 0 0.25em #fff, 0 0 0.3em #fff, 0 0 0.3em #fff, 0 0 0.3em #fff",
-                                zIndex: "1000"
-                            }}
-                        >
-                            CORONA EMOTIONS CLUB &nbsp;&nbsp; CORONA EMOTIONS
-                            CLUB &nbsp;&nbsp; CORONA EMOTIONS CLUB &nbsp;&nbsp;
-                            CORONA EMOTIONS CLUB &nbsp;&nbsp;CORONA EMOTIONS
-                            CLUB &nbsp;&nbsp; CORONA EMOTIONS CLUB
-                            &nbsp;&nbsp;CORONA EMOTIONS CLUB
-                            &nbsp;&nbsp;&nbsp;&nbsp; CORONA EMOTIONS CLUB
-                            &nbsp;&nbsp;CORONA EMOTIONS CLUB &nbsp;&nbsp;
-                        </h1>
-                    )}
-                </Ticker>
+            <GoogleFontLoader
+                fonts={[
+                    {
+                        font: "Codystar",
+                        weights: [400, "400i"]
+                    }
+                ]}
+            />
 
-                <nav>
-                    <NavLink
-                        className="link-menu"
-                        activeClassName="active"
-                        to="/submit"
+            <Ticker speed={4}>
+                {({ index }) => (
+                    <h1
+                        style={{
+                            fontFamily: "Codystar",
+                            textShadow:
+                                "0 0 0.1em #fff, 0 0 0.15em #fff, 0 0 0.2em #fff, 0 0 0.25em #fff, 0 0 0.3em #fff, 0 0 0.3em #fff, 0 0 0.3em #fff",
+                            zIndex: "1000"
+                        }}
                     >
-                        submit
-                    </NavLink>
-                    &nbsp;&nbsp;
-                    <NavLink
-                        className="link-menu"
-                        activeClassName="active"
-                        to="/browse"
-                    >
-                        browse
-                    </NavLink>
-                    &nbsp;&nbsp;
-                    <NavLink
-                        className="link-menu"
-                        activeClassName="active"
-                        to="/info"
-                    >
-                        info
-                    </NavLink>
-                    &nbsp;&nbsp;
-                    <NavLink
-                        className="link-menu"
-                        activeClassName="active"
-                        to="/admin"
-                    >
-                        data
-                    </NavLink>
-                    &nbsp; 🦠 &nbsp;
-                    <span className="instructions">
-                        how do you feel about the COVID-19 crisis? let it all
-                        out!
-                    </span>
-                </nav>
+                        CORONA EMOTIONS CLUB &nbsp;&nbsp; CORONA EMOTIONS CLUB
+                        &nbsp;&nbsp; CORONA EMOTIONS CLUB &nbsp;&nbsp; CORONA
+                        EMOTIONS CLUB &nbsp;&nbsp;CORONA EMOTIONS CLUB
+                        &nbsp;&nbsp; CORONA EMOTIONS CLUB &nbsp;&nbsp;CORONA
+                        EMOTIONS CLUB &nbsp;&nbsp;&nbsp;&nbsp; CORONA EMOTIONS
+                        CLUB &nbsp;&nbsp;CORONA EMOTIONS CLUB &nbsp;&nbsp;
+                    </h1>
+                )}
+            </Ticker>
 
-                <Route path="/submit" component={Submit}></Route>
+            <nav>
+                <NavLink
+                    className="link-menu"
+                    activeClassName="active"
+                    to="/submit"
+                >
+                    submit
+                </NavLink>
+                &nbsp;&nbsp;
+                <NavLink
+                    className="link-menu"
+                    activeClassName="active"
+                    to="/browse"
+                >
+                    browse
+                </NavLink>
+                &nbsp;&nbsp;
+                <NavLink
+                    className="link-menu"
+                    activeClassName="active"
+                    to="/info"
+                >
+                    info
+                </NavLink>
+                &nbsp;&nbsp;
+                <NavLink
+                    className="link-menu"
+                    activeClassName="active"
+                    to="/admin"
+                >
+                    data
+                </NavLink>
+                &nbsp; 🦠 &nbsp;
+                <span className="instructions">
+                    how do you feel about the COVID-19 crisis? let it all out!
+                </span>
+            </nav>
 
-                <Route path="/browse" component={Browse}></Route>
+            <Route path="/submit" component={Submit}></Route>
 
-                <Route path="/info" component={Info}></Route>
+            <Route path="/browse" component={Browse}></Route>
 
-                <Route path="/admin" component={Admin}></Route>
-            </>
-        );
-    }
+            <Route path="/info" component={Info}></Route>
+
+            <Route path="/admin" component={Admin}></Route>
+        </>
+    );
 }
